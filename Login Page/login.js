6601b1e26b58f788b08660ad42bfdfa062c20be9
Sasha Chen <sashachen@Sashas-MacBook-Air.local>
2026-04-21 12:57:21 -0400

    Update login.js

--- Login Page/login.js
+++ Login Page/login.js
@@ -1,10 +1,14 @@
-const VALID_USERNAMES = ['user1', 'admin1'];
+const VALID_USERNAMES = ['user1', 'admin1', 'recruiter1', 'volunteer1'];
 const USERS = {
 	'user1': { password: 'CNIT280', label: 'user1 (Client)'},
 	'admin1': { password: 'CNIT280', label: 'admin1 (Administrator)'},
 	'recruiter1': { password: 'CNIT280', label: 'recruiter1 (Recruiter)'},
-	'volunteer1': { password: 'CNIT280', label: 'volunteer1(Volunteer) '}
+	'volunteer1': { password: 'CNIT280', label: 'volunteer1(Volunteer) '},
 };
+
+const VALID_USERNAME = 'user1';
+const VALID_PASSWORD = 'CNIT280';
+
 
 function validateLogin(event) {
 	event.preventDefault();
@@ -13,15 +17,17 @@ function validateLogin(event) {
 	const password = document.getElementById('password').value;
 	const errorMessage = document.getElementById('errorMessage');
 
-	if (VALID_USERNAMES.includes(username) && USERS[username] && USERS[username].password === password) {
+	if (VALID_USERNAMES.includes(username) && password === VALID_PASSWORD) {
 		localStorage.setItem('whcfUser', username);
 	if (USERS[username] && USERS[username].password === password) {
 		sessionStorage.setItem('loggedInUser', USERS[username].label)
 		window.location.href = '../Home Page/index.html';
 		return false;
 
+	
 	}
 
 	errorMessage.style.display = 'block';
 	return false;
+}
 }
