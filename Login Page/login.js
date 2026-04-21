0570b14af2271157757c11b13b06491206453e39
Sasha Chen <sashachen@Sashas-MacBook-Air.local>
2026-04-21 12:30:59 -0400

    database changes

--- Login Page/login.js
+++ Login Page/login.js
@@ -1,14 +1,5 @@
-const VALID_USERNAMES = ['user1', 'admin1', 'recruiter1', 'volunteer1'];
-const USERS = {
-	'user1': { password: 'CNIT280', label: 'user1 (Client)'},
-	'admin1': { password: 'CNIT280', label: 'admin1 (Administrator)'},
-	'recruiter1': { password: 'CNIT280', label: 'recruiter1 (Recruiter)'},
-	'volunteer1': { password: 'CNIT280', label: 'volunteer1(Volunteer) '},
-};
-
 const VALID_USERNAME = 'user1';
 const VALID_PASSWORD = 'CNIT280';
-
 
 function validateLogin(event) {
 	event.preventDefault();
@@ -17,13 +8,9 @@ function validateLogin(event) {
 	const password = document.getElementById('password').value;
 	const errorMessage = document.getElementById('errorMessage');
 
-	if (VALID_USERNAMES.includes(username) && password === VALID_PASSWORD) {
-		localStorage.setItem('whcfUser', username);
-	if (USERS[username] && USERS[username].password === password) {
-		sessionStorage.setItem('loggedInUser', USERS[username].label)
+	if (username === VALID_USERNAME && password === VALID_PASSWORD) {
 		window.location.href = '../Home Page/index.html';
 		return false;
-
 	}
 
 	errorMessage.style.display = 'block';
